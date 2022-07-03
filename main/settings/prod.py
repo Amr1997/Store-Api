fe2ca4b049a9521bf6f05c8b3c2c370687e5061b
Amr1997 <93472846+Amr1997@users.noreply.github.com>
2022-07-03 19:23:32 +0200

    deployment

---
+++ main/settings/prod.py
@@ -0,0 +1,14 @@
+import os
+from .common import *
+import dj_database_url
+
+DEBUG = False
+
+SECRET_KEY = os.environ['SECRET_KEY']
+
+ALLOWED_HOSTS = ['onlinestore-prod.herokuapp.com']
+
+
+DATABASES = {
+    'default': dj_database_url.config()
+}
