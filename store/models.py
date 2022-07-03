0c2fa585470bd3272ce9b530a1678738a426599a
Amr1997 <93472846+Amr1997@users.noreply.github.com>
2022-07-03 19:58:48 +0200

    removed uuid

--- store/models.py
+++ store/models.py
@@ -126,7 +126,7 @@ class Address(models.Model):
 
 
 class Cart(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid4)
+    
     created_at = models.DateTimeField(auto_now_add=True)
 
 
