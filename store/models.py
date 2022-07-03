130451b0db1f6a927897e9450fd16c64b34ad56f
Amr1997 <93472846+Amr1997@users.noreply.github.com>
2022-07-03 19:48:58 +0200

    fix cart uuid migration r

--- store/models.py
+++ store/models.py
@@ -126,7 +126,7 @@ class Address(models.Model):
 
 
 class Cart(models.Model):
-    
+    id = models.UUIDField(primary_key=True,default=uuid4,unique=True )
     created_at = models.DateTimeField(auto_now_add=True)
 
 
